8403d135bfc3d64a64b92b240cd45db8f0c93351
raphaelchikuka-young-machine <raphaelchikuka@gmail.com>
2026-04-14 16:56:24 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'sb_publishable_mwK9QV5WLjvPgho-A2UAbw_h9qK9IBU'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://vhhvypsjmswjfnmdtady.supabase.co'
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoaHZ5cHNqbXN3amZubWR0YWR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNTU2MzksImV4cCI6MjA5MTczMTYzOX0.xMwdozKgEUeENyOKvbfmoQu1Hnitw6yf2ucLBavEJNo'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
